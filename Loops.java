34e364f40bed1ce9e4ca90dd86fb07b751a43a80
rchuhi <94059813+rchuhi@users.noreply.github.com>
2021-11-29 16:29:29 -0500

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,20 @@
+/*Reginah Chuhi
+CIS219
+Tuesday and Thursday 3:30pm*/
+public class Loops {
+
+	public static void main(String[] args) {
+		
+		int myNum=40; 
+	     if (myNum<5)
+	        
+	     System.out.println("Small");
+	     else if(myNum<=30)
+	         System.out.println("Medium");
+	     else 
+	            System.out.println("Large");
+			}
+	
+	
+		
+}
