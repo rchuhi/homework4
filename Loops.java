3e276c6ac6ed2fd6654938090aff363dc2940928
rchuhi <94059813+rchuhi@users.noreply.github.com>
2021-11-29 16:48:18 -0500

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,29 @@
+/*Reginah Chuhi
+CIS219
+Tuesday and Thursday 3:30pm*/
+public class Loops {
+
+	public static void main(String[] args) {
+		//Creates a method called myNum
+		int myNum=7; 
+		//creates an if loop
+	     if (myNum<=5)
+	        /*prints out small when the value is less 
+	         than or equal to 5
+	         */
+	     System.out.println("Small");
+	     /*calls the method again and checks if 
+	       the value is less than or equal to 8
+	      */
+	     else if(myNum<=8)
+	         System.out.println("Medium");
+	     else 
+	    	 /*If the value doesn't meet the above 
+	    	  parameter it prints Large
+	    	  */
+	            System.out.println("Large");
+			}
+	
+	
+		
+}
